94f89d9286f7345ea3fb03906325ba4541229493
liquid1188 <arlikoudis@gmail.com>
2026-09-09 05:33:48 +0000

    Move Frank La Rocca to the archive pending client approval

--- script.js
+++ script.js
@@ -262,7 +262,6 @@ async function handleSubmit(e){
       dotsWrap=document.getElementById('liveDots'),
       imgs=[].slice.call(shots.querySelectorAll('img'));
   var data=[
-    ['https://liquid1188.github.io/frank-larocca-site/','franklarocca.com'],
     ['https://www.traddyland.com/','traddyland.com'],
     ['https://liquid1188.github.io/urban-sophisticate-collective/','urbansophisticatecollective.com'],
     ['https://brushandsoulstudio.com/','brushandsoulstudio.com'],
